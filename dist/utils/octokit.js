d1cd29807112a109f75364af3484cb77692d94a7
UWENAYO Alain Pacifique <66881609+uwenayoallain@users.noreply.github.com>
2022-06-15 20:06:05 +0000

    update

---
+++ dist/utils/octokit.js
@@ -0,0 +1,8 @@
+import { Octokit } from "@octokit/core";
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN, });
+async function getUsers() {
+    const response = await octokit.request("GET /users", { headers: { "Content-Type": "application/json" } });
+    return response.data;
+}
+export default octokit;
+export { getUsers };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("yarison", this)
+import { getUsers } from "./utils/octokit";
+
+const users = await getUsers();
+console.log(users);
