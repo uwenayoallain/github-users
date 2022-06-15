c03294a9a628c6aef3b164032978b32b0d737027
UWENAYO Alain Pacifique <66881609+uwenayoallain@users.noreply.github.com>
2022-06-15 18:21:56 +0000

    first commit

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 144:
+/***/ (function() {
+
+console.log("yarison", this);
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__[144]();
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
